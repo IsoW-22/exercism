7e563c4b26e2ed180c363d8a05331f79310f9f8f
Mostafa Zeighami <105848369+IsoW-22@users.noreply.github.com>
2022-11-20 22:50:41 +0330

    Create solution.js

---
+++ Resistor Color/solution.js
@@ -0,0 +1,13 @@
+export const colorCode = (Cc) => { return COLORS.indexOf(Cc); };
+export const COLORS = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+  ];
